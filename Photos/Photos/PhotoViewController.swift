186170bdb4ccb3aa76c786f754b2ab35daaac337
Max Taylor <maxwell97taylor@gmail.com>
2019-03-15 23:17:50 -0500

    Finished app

---
+++ Photos/Photos/PhotoViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PhotoViewController.swift
+//  Photos
+//
+//  Created by Max Taylor on 3/15/19.
+//  Copyright © 2019 Max Taylor. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cameraButton(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+
+    @IBAction func libraryButton(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        imageView.image = image
+        dismiss(animated:true, completion: nil)
+    }
+}
